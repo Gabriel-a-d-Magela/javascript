3b82f642694888d04a56281132963fa6a404e842
Gabriel Anthony <108341232+Gabriel-a-d-Magela@users.noreply.github.com>
2023-11-22 18:38:46 -0300

    Exercício da tabuada concluído

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Olá')
+function gerarTabuada() {
+    var n1 = document.getElementById('txtn1')
+    var tab = document.getElementById('seltab')
+    if (n1.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        n1 = Number(n1.value)
+        tab.innerHTML = ''
+        for (cont=1;cont<=10;cont++) {
+            var item = document.createElement('option')
+            item.text = `${n1} x ${cont} = ${n1*cont}`
+            item.value = `tab${cont}`
+            tab.appendChild(item)
+    }  
+    }
+}
