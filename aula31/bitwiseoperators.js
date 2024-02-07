20a4fbeb38785e89a0e2b12410f4fc2261d562fd
Gabriel Anthony <108341232+Gabriel-a-d-Magela@users.noreply.github.com>
2024-02-07 06:42:35 -0300

    Aprendendo sobre operadores bit a bit

---
+++ aula31/bitwiseoperators.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// A tabela-verdade é muito útil para essas operações
+
+console.log(9 & 15); // 9 = 1001
+console.log(9 | 15); // 15 = 1111
+console.log(9 ^ 15); // 6 = 0110
+console.log(~9);
+console.log(~15);
+console.log(9 << 2);
+console.log(9 >> 2);
+console.log(15 >>> 2);
