5c99d2b168069135af4b6cbc9edb3c7d8714f421
Gabriel Anthony <108341232+Gabriel-a-d-Magela@users.noreply.github.com>
2024-07-20 19:04:16 -0300

    Tentando evitar repetições nos valores

--- aula36/script.js
+++ aula36/script.js
@@ -28,6 +28,7 @@ function apostar() {
     let quarnum = document.getElementById("quarto_numero");
     let quinum = document.getElementById("quinto_numero");
     let sexnum = document.getElementById("sexto_numero");
+    /* Testa se os inputs estão vazios */
     if (prinum.value.length == 0 || secnum.value.length == 0 || ternum.value.length == 0 || quarnum.value.length == 0 || quinum.value.length == 0 || sexnum.value.length == 0) { /* O 0 é valor não digitado*/ 
         alert("[ERRO] Digite um valor!");
     } else {
@@ -37,6 +38,7 @@ function apostar() {
         quarnum = Number(quarnum.value);
         quinum = Number(quinum.value);
         sexnum = Number (sexnum.value);
+        /* Testa se os valores estão na faixa de valores correta */
         if (prinum < 1 || prinum > 60) {
             erro();
         } else if (secnum < 1 || secnum > 60) {
@@ -50,15 +52,25 @@ function apostar() {
         } else if (sexnum < 1 || sexnum > 60) {
             erro();
         } else {
-            if (numAposta.indexOf(prinum) == -1) {
+            /* Testa se o array está vazio */
+            /* Refazer o funcionamento do array */
+            if (numAposta.length === 0) {
                 numAposta.push(prinum);
                 numAposta.push(secnum);
                 numAposta.push(ternum);
                 numAposta.push(quarnum);
                 numAposta.push(quinum);
                 numAposta.push(sexnum);
+                };
+            if (numAposta[0] == numAposta[1]) {
+                alert("[ERRO] Os valores não podem ser repetidos!");
+                for (let i = 0; i < 6;i++) {
+                    numAposta.pop();
+                };
+            } else {
+                gerarNumerodaSorte();
             };
+            };
+            
         };
-    gerarNumerodaSorte();
-    };
 };
